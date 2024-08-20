787bc30f685a06a1bcf50b26f544ac9390e2994d
Michał Śledź <michalsledz34@gmail.com>
2024-08-20 21:11:03 +0200

    Hide menu after clicking an option

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.getElementById("navbar-menu").onclick = () => {
+function toggleMenu() {
     const menuContent = document.getElementById("navbar-menu-content");
     if (window.getComputedStyle(menuContent).display !== "none") {
         menuContent.style.display = "none";
     } else {
         menuContent.style.display = "flex";
     }
-};
+}
+
+document.getElementById("navbar-menu").onclick = toggleMenu;
+document.getElementById("navbar-menu-list").onclick = toggleMenu;
